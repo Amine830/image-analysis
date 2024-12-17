0b8ebe2fcd3c317176102795c8f487062944d1e8
Amine830 <162301827+Amine830@users.noreply.github.com>
2024-12-17 22:51:05 +0100

    Delete .qt directory

--- .qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_QtProject_FILE /mnt/e/Univ/M1/Image/image-analysis/QtProject/QtProject)
-set(__QT_DEPLOY_TARGET_QtProject_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_OpenCVApp_FILE /mnt/e/Univ/M1/Image/image-analysis/OpenCVApp)
-set(__QT_DEPLOY_TARGET_OpenCVApp_TYPE EXECUTABLE)
